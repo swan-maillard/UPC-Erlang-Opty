e1555d44ead61c1453f80064b4904110116565a1
Swan <maillard.swan@gmail.com>
2023-10-24 11:24:28 +0200

    Distributed nodes

--- opty.erl
+++ opty.erl
@@ -1,15 +1,16 @@
 -module(opty).
 -export([start/5, stop/1]).
 
+-define(SRV, 'opty-srv@LAPTOP-MC47CKNN').
+
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Reads: Number of read operations per transaction
 %% Writes: Number of write operations per transaction
 %% Time: Duration of the experiment (in secs)
 
-
 start(Clients, Entries, Reads, Writes, Time) ->
-    register(s, server:start(Entries)),
+    spawn(?SRV, fun() -> register(s, server:start(Entries)) end),
     L = startClients(Clients, [], Entries, Reads, Writes),
     io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n",
               [Clients, Entries, Reads, Writes, Time]),
@@ -21,13 +22,13 @@ stop(L) ->
     io:format("Stopping...~n"),
     stopClients(L),
     waitClients(L),
-    s ! stop,
+    {s, ?SRV} ! stop,
     io:format("Stopped~n").
 
 
 startClients(0, L, _, _, _) -> L;
 startClients(Clients, L, Entries, Reads, Writes) ->
-    Pid = client:start(Clients, Entries, Reads, Writes, s),
+    Pid = client:start(Clients, Entries, Reads, Writes, {s, ?SRV}),
     startClients(Clients - 1, [Pid | L], Entries, Reads, Writes).
 
 
